9bd7c315da0735c9df9a60df3156e792e876374d
codingandmore <>
2025-01-19 21:09:54 +0100

    add fillet to circle

--- absaug.py
+++ absaug.py
@@ -11,32 +11,39 @@ def construct():
             t = Triangle(c=50, a=50, B=90, align=Align.MIN)
             hyp = t.edge_b
             
-        extrude(amount=length)
+        extrude(amount=length / 2)
+
+        # Drill hole for vacuum hose
         f = part.faces().sort_by(SortBy.AREA)[-1]     
         edge_x0 = part.edges().sort_by(Axis.Y).sort_by(Axis.Z)[0]
         with BuildSketch(f):
-            Circle(dm_absaug / 2)
-        
+            with Locations((0, -length/4)):
+                Circle(dm_absaug / 2)        
         extrude(amount=-f.distance(edge_x0), mode=Mode.SUBTRACT)
         
+        # make a cutout for the screw holes
         with BuildSketch(Plane.XY.offset(10)):
-            with Locations((30, 25, 0)):
+            x_off = (length - dm_absaug) / 4 + dm_absaug / 2
+            with Locations((x_off, 25, 0)):
                 Rectangle(20, 30)
-        
         extrude(amount=t.a - 10, mode=Mode.SUBTRACT)  
-        # hole_face =np.faces().sort_by(Axis.Z)[-1]          
-        # with Locations(hole_face):      
-        #     Hole(4)
         
+        # Drill the horizontal hole
         hole_face = part.faces().filter_by(Plane.XY).sort_by(Axis.Z)[-1]
-
-        with Locations(hole_face):      
+        with Locations(hole_face):     
+            CounterSinkHole(screw_hole_diameter, screw_hole_diameter * 2) 
             Hole(screw_hole_diameter)
 
+        # Drill the vertical hole
         hole_face = part.faces().filter_by(Plane.XZ).sort_by(Axis.Y)[-1]
         with Locations(hole_face):      
-            Hole(screw_hole_diameter)
+            CounterSinkHole(screw_hole_diameter, screw_hole_diameter * 2) 
 
+        c = part.edges().filter_by(GeomType.CIRCLE).sort_by(Axis.Y)[-1]
+        fillet(c, radius=4)
+
+        # mirror the part to get full length
+        mirror(about = Plane.YZ)
     show_all()
 
 if __name__ == '__main__':
