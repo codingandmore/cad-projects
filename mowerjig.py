1b0af6253098124b18bc86b016bf0b1d267119da
codingandmore <>
2025-01-22 09:22:48 +0100

    mower: extend vertical

--- mowerjig.py
+++ mowerjig.py
@@ -10,32 +10,39 @@ def construct() -> Part:
     guide_len = 70
 
     with BuildPart() as rb:
+        # Slider Bar Front Side
         with BuildSketch(Plane.XZ):
             Rectangle(length, bar_width)
 
         extrude(amount=thickness)
         front_face = rb.faces().sort_by(Axis.Y)[0]
+        # cut out the slot
         with BuildSketch(front_face):
             slot_lengh = length - 30
             with Locations((-5, 0)):
                 SlotCenterToCenter(slot_lengh, 5)
         extrude(amount=-thickness, mode=Mode.SUBTRACT)
-        # with BuildSketch(front_face):
-        #     with Locations((length / 2 - bar_width_sm, bar_width / 2)):
-        #         Rectangle(bar_width_sm, height - bar_width, align=(Align.MIN, Align.MIN))
-        # extrude(amount=-thickness)
+
+        # extend slider to lowest z
+        with BuildSketch(front_face):
+            with Locations((0, -((bar_width+thickness) / 2))):            
+                Rectangle(length, thickness)
+        extrude(amount=-thickness)
         
+        # guide wall to the right
+        lowest_z = -bar_width / 2 - thickness
         with BuildSketch(Plane.YZ.offset(length/2)):
-            with Locations((-thickness, -bar_width / 2)):
+            with Locations((-thickness, lowest_z)):
                 Rectangle(guide_len, height, align=(Align.MIN, Align.MIN))
         extrude(amount=-thickness)
 
-        # mount for vise:
+        # mount for vise at bottom:
         with BuildSketch(front_face):
-            with Locations((length / 2 - bar_width, - bar_width / 2)):
+            with Locations((length / 2 - bar_width, lowest_z)):
                 Rectangle(bar_width, thickness, align=(Align.MIN, Align.MIN))
+            mirror(about=Plane.YZ)
         extrude(amount=-bar_width) 
-        ff = rb.faces(Select.LAST).filter_by(Plane.XY).sort_by(Axis.Z)[1]
+        ff = rb.faces(Select.LAST).filter_by(Plane.XY).sort_by(Axis.Z)[1:3]
         with Locations(ff):
             CounterSinkHole(3, counter_sink_radius=6)
         
