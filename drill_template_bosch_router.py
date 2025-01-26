091c10a1be1d8efc21e427c5a68564344602633a
codingandmore <>
2025-01-26 23:09:22 +0100

    drill template: add dimensions

--- drill_template_bosch_router.py
+++ drill_template_bosch_router.py
@@ -6,40 +6,43 @@ width = 8
 thickness = 2
 angle = 0
 diameter_circle = 103
-hole_diameter = 4
+hole_diameter = 2
 slot_offset = 5
 
 three_hole_radius = 58.5
 two_hole_radius = 67
-height_adjustment_radius = 64.5
+height_adjustment_radius = 65.5
 
 with BuildPart() as part:
     with BuildSketch() as sketch:
-        Circle(diameter_circle / 2)       
-        Circle(diameter_circle / 2 - 4, mode=Mode.SUBTRACT) 
+        Circle(diameter_circle / 2)
+        Circle(diameter_circle / 2 - 4, mode=Mode.SUBTRACT)
         # height adjustment screw
-        with Locations(Rot(Z=-30)): # perform a rotation coplanar with `Plane.XY               
+        with Locations(Rot(Z=-28)): # perform a rotation coplanar with `Plane.XY
             length = height_adjustment_radius + slot_offset
             SlotOverall(length, width, align=(Align.MIN, Align.CENTER))
             with Locations((length-5, 0)):
                 Circle(hole_diameter / 2, mode=Mode.SUBTRACT)
         # three hole arms:
-        with PolarLocations(0, 3, start_angle=30):
+        with PolarLocations(0, 3, start_angle=34.5):
             length = three_hole_radius + slot_offset
             with Locations((0, 0)):
                 SlotOverall(length, width, align=(Align.MIN, Align.CENTER))
             with Locations((three_hole_radius, 0)):
-                Circle(hole_diameter / 2, mode=Mode.SUBTRACT)        
+                Circle(hole_diameter / 2, mode=Mode.SUBTRACT)
         # two hole arms:
-        with PolarLocations(0, 2, start_angle=50):
+        with PolarLocations(0, 2, start_angle=26):
             length = two_hole_radius + slot_offset
             with Locations((0, 0)):
                 SlotOverall(length, width, align=(Align.MIN, Align.CENTER))
             with Locations((two_hole_radius, 0)):
-                Circle(hole_diameter / 2, mode=Mode.SUBTRACT)  
-        # center hole:
+                Circle(hole_diameter / 2, mode=Mode.SUBTRACT)
+        # center hole outer circle:
         with Locations((0, 0)):
-            Circle(hole_diameter / 2, mode=Mode.SUBTRACT)     
+            Circle(hole_diameter / 2, mode=Mode.SUBTRACT)
+        # center hole inner circle:
+        with Locations((-2, 0)):
+            Circle(hole_diameter / 2, mode=Mode.SUBTRACT)
     extrude(amount=thickness)
-    
+
 show_all()
