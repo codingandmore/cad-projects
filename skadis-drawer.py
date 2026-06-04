50c4a1b377c514c5a0e2ccb59fda0775e4156289
codingandmore <>
2026-06-04 12:31:12 +0200

    connect wall parts, make assembly

--- skadis-drawer.py
+++ skadis-drawer.py
@@ -1,3 +1,4 @@
+# %%
 import copy
 from build123d import *
 from ocp_vscode import show_clear,show, show_all, Camera
@@ -46,8 +47,8 @@ def create_hook_90():
 
 def create_snap_hook():
     mid_point_y = wall_height / 2
-    with BuildPart() as part:
-        with BuildSketch(Plane.XY):
+    with BuildPart() as partBuilder:
+        with BuildSketch(Plane.XZ):
             with Locations((0, mid_point_y)):
                 Rectangle(0.2, 2, align=(Align.MIN, Align.MIN))
             with Locations((0, mid_point_y + 2)):
@@ -63,25 +64,65 @@ def create_snap_hook():
                 Line(triangle_bottom_left, triangle_bottom_right)
             make_face()
         half_hook = extrude(amount=wall_thickness - 2)
-        half_hook = fillet(half_hook.edges().filter_by(Axis.Z).sort_by(Axis.Y)[-1], 1.0)
-        half_hook = fillet(half_hook.faces().filter_by(Axis.X).sort_by(Axis.X)[-1].edges(), 0.5)
-        mirror_plane = Plane.XZ.offset(-mid_point_y)
+        half_hook.edges().filter_by(Axis.Y).sort_by(Axis.Z)[-1]
+        half_hook = fillet(half_hook.edges().filter_by(Axis.Y).sort_by(Axis.Z)[-1], 1.0)
+        half_hook = fillet(half_hook.faces().filter_by(Axis.X).sort_by(Axis.X)[-1].edges(), 0.5)        # add support bar
+        with BuildSketch(Plane.XZ):
+            with Locations((4, mid_point_y)):
+                Rectangle(1, 4, align=(Align.MIN, Align.MIN))
+        extrude(amount=-1)
+
+    half_hook = partBuilder.part
+    mirror_plane = Plane.XY.offset(mid_point_y)
+    with BuildPart() as partBuilder:
+        add(half_hook)
         mirror(half_hook, mirror_plane)
-    return part
+        RigidJoint(label="hookjt", joint_location=Location((0, -(wall_thickness-2)/2, mid_point_y)))
+
+    return partBuilder.part
 
 def create_snap_groove(from_snap_hook: Part):
     with BuildPart() as outer_hook:
-        offset(snap_hook.part, amount=0.3)
-    return outer_hook
+        offset(from_snap_hook.part, amount=0.3)
+    return outer_hook.part
 
 def create_wall(x_units: int, y_units: int, len_units: int):
-    with BuildPart() as wall:
+    # main part:
+    loc = (x_units * slot_spacing, y_units * slot_spacing)
+    with BuildPart() as builder:
         with BuildSketch(Plane.XY):
-            with Locations((x_units * slot_spacing, y_units * slot_spacing)):
+            with Locations(loc):
                 Rectangle(skadis_slot_w, len_units * slot_spacing, align=(Align.CENTER, Align.MIN))
-
         extrude(amount=wall_height)
-    return wall
+        joint_loc = faces().filter_by(Plane.XZ).sort_by(Axis.Y)[-1].center_location
+        print(joint_loc)
+        joint_loc.orientation = (0, 0, 90)
+        print(joint_loc)
+        RigidJoint(label="wallendjt", joint_location=joint_loc)
+    wall = builder.part
+
+    # start extension:
+    loc = (loc[0], loc[1]-5)
+    with BuildPart() as builder:
+        with BuildSketch(Plane.XY):
+            with Locations(loc):
+                Rectangle(5, 5, align=(Align.CENTER, Align.MIN))
+        extrude(amount=wall_height)
+    start_block = builder.part
+
+    # end extension:
+    # loc = (loc[0], loc[1] + len_units * slot_spacing)
+    # with BuildPart() as builder:
+    #     with BuildSketch(Plane.XY):
+    #         with Locations(loc):
+    #             Rectangle(5, 5, align=(Align.CENTER, Align.MIN))
+    #     extrude(amount=wall_height)
+
+    # create hook as end
+    snap_hook = create_snap_hook()
+    wall.joints["wallendjt"].connect_to(snap_hook.joints["hookjt"])
+    wall_assembly = Compound(label="wall", children=[wall, start_block, snap_hook])
+    return wall_assembly
 
 def export (parts: Iterable[Part], name: str):
     exporter = Mesher()
@@ -98,27 +139,33 @@ def export (parts: Iterable[Part], name: str):
     )
     exporter.write(name)
 
+# %%
 if __name__ == '__main__':
     show_clear()
-    hook = create_hook()
-    hook.label = "hook"
-    hook90 = create_hook_90()
-    hook90.label = "hook90"
-    snap_hook = create_snap_hook()
-    snap_hook.label = "SnapHook"
-    outer_hook = create_snap_groove(snap_hook)
-    outer_hook.label = "SnapHookGroove"
+    # hook = create_hook()
+    # hook.label = "hook"
+    # hook90 = create_hook_90()
+    # hook90.label = "hook90"
+    # snap_hook = create_snap_hook()
+    # snap_hook.label = "SnapHook"
+    # outer_hook = create_snap_groove(snap_hook)
+    # outer_hook.label = "SnapHookGroove"
     wall = create_wall(2, 3, 5)
     wall.label = "wall-1"
-
-    # show_objects = (
-    #     # hook,
-    #     # hook90,
-    #     # snap_hook,
-    #     # outer_hook,
-    #     wall,
-    # )
-    # show(show_objects, reset_camera=Camera.KEEP)
-    show_all()
+    # sb.label = "sb"
+    # eb.label = "eb"
+    show_objects = (
+        # hook,
+        # hook90,
+        # snap_hook,
+        # outer_hook,
+        wall,
+        # sb,
+        # eb,
+    )
+    show(show_objects, reset_camera=Camera.KEEP, render_joints=True)
+    # show_all()
     # export([part1, part2], "drawer-1.3mf")
 
+
+# %%
