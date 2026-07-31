437f924e4d95e1d138ecfd4360f7873f87ff25a8
codingandmore <>
2026-07-31 15:25:49 +0200

    drawer v2 with adapters

--- skadis_drawer_1.py
+++ skadis_drawer_1.py
@@ -27,32 +27,37 @@ if __name__ == '__main__':
     board = org.create_board(27, 17, border_x=10, border_y=10, start_indented=True)
     board.label = "Board"
 
-    walla = org.create_wall(2, 5, 9, SkadisOrganizer.Orientation.VERTICAL)
+    walla = org.create_wall(2, 5, 9, SkadisOrganizer.Orientation.VERTICAL, True)
     walla.label = "A"
 
-    wallb = org.create_wall(4, 5, 6, SkadisOrganizer.Orientation.VERTICAL)
+    wallb = org.create_wall(4, 5, 6, SkadisOrganizer.Orientation.VERTICAL, True)
     wallb.label = "B"
 
-    wallc = org.create_wall(6, 5, 4, SkadisOrganizer.Orientation.VERTICAL)
+    wallc = org.create_wall(6, 5, 4, SkadisOrganizer.Orientation.VERTICAL, True)
     wallc.label = "C"
-    walld = org.create_wall(13, 5, 6, SkadisOrganizer.Orientation.VERTICAL)
+    walld = org.create_wall(13, 5, 6, SkadisOrganizer.Orientation.VERTICAL, True)
     walld.label = "D"
-    walle = org.create_wall(16, 2, 12, SkadisOrganizer.Orientation.VERTICAL)
+    walle = org.create_wall(16, 2, 12, SkadisOrganizer.Orientation.VERTICAL, True)
     walle.label = "E"
     wallf = org.create_wall(18, 2, 12, SkadisOrganizer.Orientation.VERTICAL)
     wallf.label = "F"
-
-    wallg = org.create_wall(2, 14, 14, SkadisOrganizer.Orientation.HORIZONTAL)
+    wallg = org.create_wall(2, 14, 12, SkadisOrganizer.Orientation.HORIZONTAL, True)
     wallg.label = "G"
-    wallh = org.create_wall(2, 5, 11, SkadisOrganizer.Orientation.HORIZONTAL)
-    wallh.label = "H"
+    adapterh1 = org.create_adapter(2, 5, 2)
+    adapterh1.label = "H1A"
+    adapterh2 = org.create_adapter(4, 5, 2)
+    adapterh2.label = "H2A"
+    adapterh3 = org.create_adapter(6, 5, 2)
+    adapterh3.label = "H3A"
+    wallh3 = org.create_wall(8, 5, 5, SkadisOrganizer.Orientation.HORIZONTAL)
+    wallh3.label = "H3"
     walli = org.create_wall(2, 2, 14, SkadisOrganizer.Orientation.HORIZONTAL)
     walli.label = "I"
-    wallj = org.create_wall(16, 14, 9, SkadisOrganizer.Orientation.HORIZONTAL)
+    wallj = org.create_wall(14, 14, 11, SkadisOrganizer.Orientation.HORIZONTAL, True)
     wallj.label = "J"
-    wallk = org.create_wall(6, 9, 7, SkadisOrganizer.Orientation.HORIZONTAL)
+    wallk = org.create_wall(6, 9, 7, SkadisOrganizer.Orientation.HORIZONTAL, True)
     wallk.label = "k"
-    walll = org.create_wall(4, 11, 9, SkadisOrganizer.Orientation.HORIZONTAL)
+    walll = org.create_wall(4, 11, 9, SkadisOrganizer.Orientation.HORIZONTAL, True)
     walll.label = "L"
     # show(show_objects, reset_camera=Camera.KEEP, render_joints=True)
     show_all(reset_camera=Camera.KEEP)
