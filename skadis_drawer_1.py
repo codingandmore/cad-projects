54b3c74cc0b47b7cb410ca242ee1faef9966ec77
codingandmore <>
2026-08-02 20:35:34 +0200

    update to latest changes

--- skadis_drawer_1.py
+++ skadis_drawer_1.py
@@ -9,7 +9,16 @@ def export (parts: Iterable[Part], name: str):
     exporter = Mesher()
 
     for part in parts:
-        exporter.add_shape(part)
+        if isinstance(part, Compound):
+            print(f'melting compound {part.label}')
+            with BuildPart() as builder:
+                for p in part.compounds():
+                    add(p)
+            x = builder.part
+            x.orientation += (0, 90, 0)
+            exporter.add_shape(x, mesh_type=MeshType.MODEL)
+        else:
+            exporter.add_shape(part, mesh_type=MeshType.MODEL)
 
     exporter.add_meta_data(
         name_space="custom",
@@ -22,46 +31,32 @@ def export (parts: Iterable[Part], name: str):
 
 
 if __name__ == '__main__':
+    org = SkadisOrganizer(wall_height=30, first_row_indented=True, with_support=True)
+    board = org.create_board(27, 17, border_x=10, border_y=10, name="Board")
+    walla = org.create_wall(2, 5, 9, SkadisOrganizer.Orientation.VERTICAL, True, "A")
+    wallb = org.create_wall(4, 5, 6, SkadisOrganizer.Orientation.VERTICAL, True, "B")
+    wallc = org.create_wall(6, 5, 4, SkadisOrganizer.Orientation.VERTICAL, True, "C")
+    walld = org.create_wall(13, 5, 6, SkadisOrganizer.Orientation.VERTICAL, True, "D")
+    walle = org.create_wall(16, 2, 12, SkadisOrganizer.Orientation.VERTICAL, True, "E")
+    wallf = org.create_wall(18, 2, 12, SkadisOrganizer.Orientation.VERTICAL, False, "F")
+    wallg = org.create_wall(2, 14, 12, SkadisOrganizer.Orientation.HORIZONTAL, True, "G")
+    adapterh1 = org.create_adapter(SkadisOrganizer.AdapterType.LEFT_HOOK, 2, 5, 2, "H1A")
+    adapterh2 = org.create_adapter(SkadisOrganizer.AdapterType.LEFT_HOOK, 4, 5, 2, "H2A")
+    adapterh3 = org.create_adapter(SkadisOrganizer.AdapterType.LEFT_HOOK, 6, 5, 2, "H3A")
+    wallh = org.create_wall(8, 5, 5, SkadisOrganizer.Orientation.HORIZONTAL, False, "H")
+    walli = org.create_wall(2, 2, 14, SkadisOrganizer.Orientation.HORIZONTAL, False, "I")
+    wallj = org.create_wall(14, 14, 11, SkadisOrganizer.Orientation.HORIZONTAL, False, "J")
+    wallk = org.create_wall(6, 9, 7, SkadisOrganizer.Orientation.HORIZONTAL, True, "K")
+    walll = org.create_wall(4, 11, 8, SkadisOrganizer.Orientation.HORIZONTAL, True, "L")
+    adapterl1 = org.create_adapter(SkadisOrganizer.AdapterType.RIGHT_GROOVE, 12, 11, 1, "L1A")
     show_clear()
-    org = SkadisOrganizer(wall_height=30)
-    board = org.create_board(27, 17, border_x=10, border_y=10, start_indented=True)
-    board.label = "Board"
-
-    walla = org.create_wall(2, 5, 9, SkadisOrganizer.Orientation.VERTICAL, True)
-    walla.label = "A"
-
-    wallb = org.create_wall(4, 5, 6, SkadisOrganizer.Orientation.VERTICAL, True)
-    wallb.label = "B"
-
-    wallc = org.create_wall(6, 5, 4, SkadisOrganizer.Orientation.VERTICAL, True)
-    wallc.label = "C"
-    walld = org.create_wall(13, 5, 6, SkadisOrganizer.Orientation.VERTICAL, True)
-    walld.label = "D"
-    walle = org.create_wall(16, 2, 12, SkadisOrganizer.Orientation.VERTICAL, True)
-    walle.label = "E"
-    wallf = org.create_wall(18, 2, 12, SkadisOrganizer.Orientation.VERTICAL)
-    wallf.label = "F"
-    wallg = org.create_wall(2, 14, 12, SkadisOrganizer.Orientation.HORIZONTAL, True)
-    wallg.label = "G"
-    adapterh1 = org.create_adapter(2, 5, 2)
-    adapterh1.label = "H1A"
-    adapterh2 = org.create_adapter(4, 5, 2)
-    adapterh2.label = "H2A"
-    adapterh3 = org.create_adapter(6, 5, 2)
-    adapterh3.label = "H3A"
-    wallh3 = org.create_wall(8, 5, 5, SkadisOrganizer.Orientation.HORIZONTAL)
-    wallh3.label = "H3"
-    walli = org.create_wall(2, 2, 14, SkadisOrganizer.Orientation.HORIZONTAL)
-    walli.label = "I"
-    wallj = org.create_wall(14, 14, 11, SkadisOrganizer.Orientation.HORIZONTAL, True)
-    wallj.label = "J"
-    wallk = org.create_wall(6, 9, 7, SkadisOrganizer.Orientation.HORIZONTAL, True)
-    wallk.label = "k"
-    walll = org.create_wall(4, 11, 9, SkadisOrganizer.Orientation.HORIZONTAL, True)
-    walll.label = "L"
     # show(show_objects, reset_camera=Camera.KEEP, render_joints=True)
     show_all(reset_camera=Camera.KEEP)
-    # export([part1, part2], "drawer-1.3mf")
+    fname = "Drawer-1.3mf"
+    print(f"Exporting to {fname}")
+    # export([walla, wallb], fname)
+    export([walla, wallb, wallc, walld, walle, wallf, wallg, wallh, adapterh1,
+                   adapterh2, adapterh3, walli, wallj, wallk, walll, adapterl1], fname)
 
 
 # %%
--- skadis_main.py
+++ skadis_main.py
@@ -23,15 +23,15 @@ def export (parts: Iterable[Part], name: str):
 
 if __name__ == '__main__':
     show_clear()
-    org = SkadisOrganizer(wall_height=30, first_row_indented=True)
-    board = org.create_board(12, 8, border_x=10, border_y=10, name="Board")
+    org = SkadisOrganizer(wall_height=30, first_row_indented=True, with_support=True)
+    # board = org.create_board(12, 8, border_x=10, border_y=10, name="Board")
     wallv = org.create_wall(1, 2, 5, SkadisOrganizer.Orientation.VERTICAL, True, "wall-v")
     wallh = org.create_wall(3, 4, 4, SkadisOrganizer.Orientation.HORIZONTAL, False, "wall-h")
     adapter = org.create_adapter(SkadisOrganizer.AdapterType.LEFT_HOOK, 3, 3, 2, "A1")
     wallh1 = org.create_wall(1, 1, 3, SkadisOrganizer.Orientation.HORIZONTAL, False, "H1")
     adapter2 = org.create_adapter(SkadisOrganizer.AdapterType.RIGHT_GROOVE, 4, 1, 3, name="A2")
     show_objects = (
-        board,
+        # board,
         wallv,
         wallh1,
         adapter,
